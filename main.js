8e29474bfb21b9e76d0bd5878cc66d417ef7bcb3
Ashokkumar S <CIPL0547@colanonline.local>
2019-03-11 20:07:42 +0530

    nwe files added

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import App from './App.js';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import LandingPage from './src/common/landingPage';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+render(<BrowserRouter>
+    <Switch>
+    <Redirect exact path='/' to='/landingPage' />
+    <Route path='/landingPage' component={LandingPage} />
+    </Switch>
+</BrowserRouter>, document.getElementById('root'));
+{/* ReactDOM.render(<App />, document.getElementById('app')); */}
